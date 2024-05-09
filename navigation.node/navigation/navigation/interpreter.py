1f0009ba97bbb880176e1a0fd4ca2d191a7212ba
Giacomo Vianello <giacomo@capeanalytics.com>
2024-05-08 23:00:40 -0700

    Changes

---
+++ navigation.node/navigation/navigation/interpreter.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import subprocess
+import re
+
+_RECOGNIZED_COMMANDS = [
+    "left",
+    "right",
+    "forward",
+    "backwards",
+    "stop"
+]
+
+class Interpreter(Node):
+    """
+    Interprets and executes the commands received on the voice_commands topic.
+    """
+    def __init__(self):
+        
+        super().__init__('interpreter')
+
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.callback,
+            10
+        )
+        
+    def callback(self, msg):
+        
+        self.get_logger().info(f'Received: {msg.data}')
+        
+        for cmd in _RECOGNIZED_COMMANDS:
+
+            if cmd in msg.data:
+            
+                self.get_logger().info(f"Executing: {cmd}")
+            
+                break
+        
+        else:
+
+            self.get_logger().info("Command not recognized")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    stream_listener = StreamListener()
+    rclpy.spin(stream_listener)
+    stream_listener.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
