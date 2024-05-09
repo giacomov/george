4f11ee4943e77be7dfc7c41a0e3104c4a1b5e349
Giacomo Vianello <giacomo@capeanalytics.com>
2024-05-08 21:27:24 -0700

    Changes

--- whisper.node/voice_commands/setup.py
+++ whisper.node/voice_commands/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'voice_commands = voice_commands.stream_listener:main',
+            'stream_listener = voice_commands.stream_listener:main',
         ],
     },
 )
